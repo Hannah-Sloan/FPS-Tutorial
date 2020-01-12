b9092f80755781b01ff3d3f244e96708a38f4cc5
Hannah Sloan <caelum.sloan1@ucalgary.ca>
2020-01-12 13:40:50 -0700

    Day 1 commit

---
+++ Assets/_Project/Scripts/DayNightTransition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class DayNightTransition : MonoBehaviour
+{
+
+    public AudioMixerSnapshot snapshot;
+
+    void Start()
+    {
+        snapshot.TransitionTo(8f);
+    }
+}
---
+++ Assets/_Project/Scripts/Programming Extras/SceneLoaderComponent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderComponent : MonoBehaviour
+{
+    [SerializeField] private string sceneToLoadName;
+    [SerializeField] private bool additive;
+    [Tooltip("Will load scene even if scene is already loaded.")]
+    [SerializeField] private bool forceSceneLoad;
+
+    private void OnEnable()
+    {
+        var scen = SceneManager.GetSceneByName(sceneToLoadName);
+        if (!forceSceneLoad && scen.isLoaded)
+            return;
+        LoadSceneMode loadSceneMode = additive ? LoadSceneMode.Additive : LoadSceneMode.Single;
+        SceneManager.LoadScene(sceneToLoadName, loadSceneMode);
+    }
+}
